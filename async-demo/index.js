dfbff0a0298c139ac6841d9b1a072f3218637db7
chinmay29hub <chinmaysonawane57@gmail.com>
2023-01-17 14:55:44 +0530

    async

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,8 +1,13 @@
 console.log("Before")
-getUser(1, (user) => {
-    console.log("User :", user)
+// getUser(1, (user) => {
+//     console.log("User :", user)
 
-    getRepos(user.githubUsername, displayRepos)
+//     getRepos(user.githubUsername, displayRepos)
+// })
+
+getUser(1).then(user => {
+    console.log(user)
+    getRepos(user.githubUsername).then(repos => console.log(repos))
 })
 console.log("After")
 
@@ -10,21 +15,21 @@ function displayRepos (repos){
     console.log("Repositories : ", repos)
 }
 
-function getUser (id, callback) {
-    const p = new Promise((resolve, reject) => {
+function getUser (id) {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log("Reading a user from a database")
-            callback({ id: id, githubUsername: "chinmay29hub" })
+            resolve({ id: id, githubUsername: "chinmay29hub" })
         }, 2000)
     })
-    
 }
 
-function getRepos(username, callback) {
-
-    setTimeout(() => {
-        console.log(`Getting repos from @${username} username`)
-        callback(["repo1", "repo2", "repo3"])
-    }, 2000)
-    
+function getRepos(username) {
+    return new Promise((resolve
+        , reject) => {
+        setTimeout(() => {
+            console.log(`Getting repos from @${username} username`)
+            resolve(["repo1", "repo2", "repo3"])
+        }, 2000)
+    })
 }
